31b9b08acb01939a4d950bedacf1126e64dfbf4d
Ruby Faucher <ruby.faucher@etud.univ-jfc.fr>
2021-02-15 22:29:25 +0100

    Commutateur fonctionnel

--- ISISCapitalist/ISISCapitalistClientWeb/src/app/product/product.component.ts
+++ ISISCapitalist/ISISCapitalistClientWeb/src/app/product/product.component.ts
@@ -2,7 +2,7 @@ import { EventEmitter, Input, Output } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 import { RestserviceService } from '../restservice.service';
 import { Product } from '../world';
-import 'maths.ts';
+import { BigvaluePipe } from '../bigvalue.pipe';
 
 @Component({
   selector: 'app-product',
@@ -11,33 +11,42 @@ import 'maths.ts';
 })
 export class ProductComponent implements OnInit {
 
-  product: Product;
+  product: Product = new Product();
   server: String;
   progressbarvalue: number;
   lastupdate: number;
   _qtmulti: string;
   _money:number;
   totalCost:number;
+  numberTransformer:BigvaluePipe;
+  maxCanBuy:number;
+
+  
 
   @Input()
   set qtmulti(value: string) {
     this._qtmulti= value;
-    if(this._qtmulti&& this.product) this.calcMaxCanBuy();
+    
+    if(this._qtmulti&& this.product) this.calcQteMulti();
+    
   }
   @Input()
   set money(value: number) {
     this._money= value;
+    this.calcQteMulti();
   }
 
   @Input()
   set prod(value: Product) {
     this.product = value;
+    this.totalCost = this.product.cout;
   }
   @Output() 
   notifyProduction: EventEmitter<Product> = new EventEmitter<Product>();
 
   constructor(private service: RestserviceService) {
     this.server = service.getServer();
+    this.numberTransformer = new BigvaluePipe();
   }
   startFabrication() {
     this.product.timeleft = this.product.vitesse;
@@ -67,17 +76,34 @@ export class ProductComponent implements OnInit {
     setInterval(() => { this.calcScore(); }, 100);
     this.progressbarvalue = 0;
   }
+  calcQteMulti(){
+    let x=this.product.cout;
+    let c=this.product.croissance;
+    if(this._qtmulti=="x1"){
 
+      this.totalCost=x;
+    }
+    else if(this._qtmulti=="x10"){
+      this.totalCost=x*((1-(c**10))/(1-c));
+    }
+    else if(this._qtmulti=="x100"){
+      this.totalCost=x*((1-(c**100))/(1-c));
+    }
+    else{
+      let n=this.calcMaxCanBuy();
+      this.totalCost=x*((1-(c**n))/(1-c));
+    }
+
+  }
   calcMaxCanBuy(){
     let x=this.product.cout;
     let c=this.product.croissance;
-    let n=0;
-    this.totalCost=x;
-    while(this._money >= this.totalCost){
-      n++;
-      this.totalCost += (x*c^n);
-    }
-    return n;
+    this.maxCanBuy=Math.floor((Math.log((-(this._money*(1-c))/x)+1))/(Math.log(c)));
+    return this.maxCanBuy;
+  }
+
+  qtMultiDisplay(){
+    return this._qtmulti === "xMax" ? "x"+this.maxCanBuy : this._qtmulti;
   }
 
 }
